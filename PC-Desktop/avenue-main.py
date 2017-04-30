36fd7327bb8dada7ec7178a8e3865d3c482708d9
ssardana08 <sardana.sumit08@gmail.com>
2017-04-30 13:21:36 +0530

    Added UI functionality to connect to server

--- PC-Desktop/avenue-main.py
+++ PC-Desktop/avenue-main.py
@@ -7,6 +7,8 @@
 # WARNING! All changes made in this file will be lost!
 
 from PyQt4 import QtCore, QtGui
+import socket
+import pyautogui as pyag
 
 try:
     _fromUtf8 = QtCore.QString.fromUtf8
@@ -41,15 +43,15 @@ class Ui_MainWindow(object):
         self.btn2.setIconSize(QtCore.QSize(30, 30))
         self.btn2.setObjectName(_fromUtf8("btn2"))
         self.gridLayout_2.addWidget(self.btn2, 0, 0, 1, 1, QtCore.Qt.AlignLeft|QtCore.Qt.AlignTop)
-        self.toolButton = QtGui.QToolButton(self.centralwidget)
-        self.toolButton.setMinimumSize(QtCore.QSize(50, 50))
-        self.toolButton.setCursor(QtGui.QCursor(QtCore.Qt.PointingHandCursor))
+        self.btn3 = QtGui.QToolButton(self.centralwidget)
+        self.btn3.setMinimumSize(QtCore.QSize(50, 50))
+        self.btn3.setCursor(QtGui.QCursor(QtCore.Qt.PointingHandCursor))
         icon1 = QtGui.QIcon()
         icon1.addPixmap(QtGui.QPixmap(_fromUtf8("../ui_files/help_icon.png")), QtGui.QIcon.Normal, QtGui.QIcon.Off)
-        self.toolButton.setIcon(icon1)
-        self.toolButton.setIconSize(QtCore.QSize(30, 30))
-        self.toolButton.setObjectName(_fromUtf8("toolButton"))
-        self.gridLayout_2.addWidget(self.toolButton, 0, 1, 1, 1)
+        self.btn3.setIcon(icon1)
+        self.btn3.setIconSize(QtCore.QSize(30, 30))
+        self.btn3.setObjectName(_fromUtf8("btn3"))
+        self.gridLayout_2.addWidget(self.btn3, 0, 1, 1, 1)
         self.gridLayout.addLayout(self.gridLayout_2, 3, 0, 1, 1)
         self.gridLayout_4 = QtGui.QGridLayout()
         self.gridLayout_4.setObjectName(_fromUtf8("gridLayout_4"))
@@ -63,9 +65,11 @@ class Ui_MainWindow(object):
         self.label_2.setFont(font)
         self.label_2.setObjectName(_fromUtf8("label_2"))
         self.gridLayout_4.addWidget(self.label_2, 0, 0, 1, 1, QtCore.Qt.AlignRight|QtCore.Qt.AlignVCenter)
-        self.lineEdit = QtGui.QLineEdit(self.centralwidget)
-        self.lineEdit.setObjectName(_fromUtf8("lineEdit"))
-        self.gridLayout_4.addWidget(self.lineEdit, 0, 1, 1, 1, QtCore.Qt.AlignLeft)
+
+        # TextEdit Column
+        self.txtedit = QtGui.QLineEdit(self.centralwidget)
+        self.txtedit.setObjectName(_fromUtf8("txtedit"))
+        self.gridLayout_4.addWidget(self.txtedit, 0, 1, 1, 1, QtCore.Qt.AlignLeft)
         self.gridLayout.addLayout(self.gridLayout_4, 1, 0, 1, 1)
         self.label = QtGui.QLabel(self.centralwidget)
         self.label.setText(_fromUtf8(""))
@@ -74,6 +78,8 @@ class Ui_MainWindow(object):
         self.gridLayout.addWidget(self.label, 0, 0, 1, 1, QtCore.Qt.AlignHCenter|QtCore.Qt.AlignTop)
         self.gridLayout_3 = QtGui.QGridLayout()
         self.gridLayout_3.setObjectName(_fromUtf8("gridLayout_3"))
+
+        #Button Start
         self.btn1 = QtGui.QPushButton(self.centralwidget)
         sizePolicy = QtGui.QSizePolicy(QtGui.QSizePolicy.Preferred, QtGui.QSizePolicy.Preferred)
         sizePolicy.setHorizontalStretch(0)
@@ -107,12 +113,48 @@ class Ui_MainWindow(object):
         self.retranslateUi(MainWindow)
         QtCore.QMetaObject.connectSlotsByName(MainWindow)
 
+        self.btn1.clicked.connect(self.server)
+
     def retranslateUi(self, MainWindow):
         MainWindow.setWindowTitle(_translate("MainWindow", "Avenue", None))
         self.btn2.setText(_translate("MainWindow", "Settings", None))
-        self.toolButton.setText(_translate("MainWindow", "...", None))
+        self.btn3.setText(_translate("MainWindow", "...", None))
         self.label_2.setText(_translate("MainWindow", "IP Address", None))
         self.btn1.setText(_translate("MainWindow", "Start", None))
+
+
+    def server(self):
+
+        self.host = socket.gethostbyname(socket.gethostname())
+        self.txtedit.setText('host')
+        self.port = 5000
+        print("Server's IP Address:"+self.host)
+
+        self.mySocket = socket.socket()
+        self.mySocket.bind((self.host,self.port))
+
+        self.mySocket.listen(1)
+        self.conn, self.addr = self.mySocket.accept()
+        print ("Connection from: " + str(self.addr))
+        while True:
+            self.data = conn.recv(1024).decode()
+            if not self.data:
+                break
+            print ("from connected  user: " + str(data))
+            if (self.data == 'b!'):
+                pyag.press('left')
+            elif (self.data == 'd!'):
+                pyag.press('right')
+            elif (self.data == 'a!'):
+                pyag.hotkey('shift','f5')
+            elif (self.data == 'c!'):
+                pyag.press('esc')
+
+
+
+
+        conn.close()
+        
 
 
 if __name__ == "__main__":
